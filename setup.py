1d1cfa6ea52d3840a0ab126803b2e0f3ed363614
Yi Yu <58670828+YiYuDL@users.noreply.github.com>
2026-06-16 09:19:13 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='BEAD',
+    version='1.1.0',
+    packages=['BEAD'],
+    url='https://github.com/YiYuDL/BEAD',
+    author='Yi Yu',
+    author_email='yuyi689@gmail.com',
+   
+)
